01cc54a5e753b93eeedf78f6763f5161dff07a62
saket77 <37559928+saket77@users.noreply.github.com>
2019-04-03 22:35:13 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -2,23 +2,33 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const pathToRegexp = require('path-to-regexp');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const mongoUrl = 'mongodb://localhost:27017';
+let mongodb = null;
 
 app.use(bodyParser.json());
 //app.use(cors.json());
 
-let items = [];
-let keys = [];
-
 app.get('/blabs', (req, res) => {
 
-	const thing = items.filter(function (item){
+	/*const thing = items.filter(function (item){
 		let x = req.query.createdSince;
 		if (x==undefined) x=0;
 		return item.postTime>=x;
 	});
+	*/
+	let x = req.query.createdSince;
+		if (x==undefined) x=0;
+		mongoDb.collection('blabs')
+        .find({
+        postTime: {$gte: x}
+        }).toArray()
+        .then(function(item) {
+            res.status(200).send(item);
+        });
 
 	
-    res.status(200).send(thing);
+    //res.status(200).send(thing);
 });
 
 app.post('/blabs', (req, res) => {
@@ -28,23 +38,38 @@ app.post('/blabs', (req, res) => {
         postTime : Math.floor(Date.now()/1000),
         id : items.length + 1,
     }
-    items.push(newItem);
-    res.status(201).send(newItem);
-    
+    //items.push(newItem);
+    //res.status(201).send(newItem);
+    mongoDb.collection('blabs')
+        .insertOne(newItem)
+        .then(function(response) {
+            res.status(201).send(newItem);
+        });
 });
 app.delete('/blabs/:blabid', (req, res) => {
-	const regexp = pathToRegexp('/blabs/:blabid', keys);
+	//const regexp = pathToRegexp('/blabs/:blabid', keys);
 	
-	const id = keys[0];
+	const id = req.params;
     //console.log(`your server is running on port ${id}`);
-	items = items.filter(function (i){
+	/*items = items.filter(function (i){
        
-		return i.id!==id;
+		return i.id!==parseInt(id.blabid);
 	});
-	res.status(200).send(`deleted blab has id ${id}`);
+	*/
+	db.blabs.deleteOne({id: {$eq: id.blabid}});
+
+	res.status(200).send(`deleted blab has id ${id.blabid}`);
 
 });
+MongoClient.connect(mongoUrl, function(err, client) {
+    if (err)
+        throw err;
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000');
+    console.log("Connected successfully to server");
+   
+    mongoDb = client.db();
+
+    app.listen(3000, () => {
+        console.log('Listening on port 3000');
+    });
 });
